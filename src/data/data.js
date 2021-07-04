2fe0aba04a892b7074cc8dda5f658b1bd0a298ce
unknown <essidmohamedamin@gmail.com>
2021-07-04 12:17:21 +0100

    populate data

--- src/data/data.js
+++ src/data/data.js
@@ -3,9 +3,9 @@ const projects = [
     id: 1,
     preview: ["wcone.png", "wctwo.png", "wcthree.png", "wcfour.png"],
     info: {
-      name: "wildcampers",
+      name: "WILDCAMPERS",
       discreption:
-        "Sunt aute nulla aliquip elit. Elit ex nisi sint qui aute Lorem eiusmod duis sunt officia. Lorem amet dolor laborum incididunt eiusmod do ex sint est adipisicing sunt esse. Fugiat mollit incididunt nulla velit sint sit ut consectetur ut laboris. Sint labore laborum quis sunt sunt labore do nisi. Dolore amet eu aliquip est nulla culpa non irure pariatur.",
+        "Wildcampers is a Spanish company that allows customers to rent camper vans. However, after the challenges of the corona virus, wildcampers decided to start selling these vehicles too and my job was to build a fast website with a good SEO for them to help them on that new business.",
       stucks: [
         "next js",
         "material ui",
@@ -21,9 +21,9 @@ const projects = [
     id: 2,
     preview: ["sione.png", "sitwo.png", "sithree.png"],
     info: {
-      name: "suggestion-improvement",
+      name: "SUGGESSTION-IMPROVEMENT",
       discreption:
-        "Sunt aute nulla aliquip elit. Elit ex nisi sint qui aute Lorem eiusmod duis sunt officia. Lorem amet dolor laborum incididunt eiusmod do ex sint est adipisicing sunt esse. Fugiat mollit incididunt nulla velit sint sit ut consectetur ut laboris. Sint labore laborum quis sunt sunt labore do nisi. Dolore amet eu aliquip est nulla culpa non irure pariatur.",
+        "A big German company needed a website for their employees to give suggestions to improve the company workflow and culture. I built a platform for them that allows users to create an account, create a suggestion (and edit it or even delete it), add images to their posts to explain it more, and they can upvote the suggestions of other employees too.",
       stucks: [
         "next js",
         "material ui",
@@ -39,9 +39,9 @@ const projects = [
     id: 3,
     preview: ["vnone.png"],
     info: {
-      name: "vendly",
+      name: "VENDLY",
       discreption:
-        "Sunt aute nulla aliquip elit. Elit ex nisi sint qui aute Lorem eiusmod duis sunt officia. Lorem amet dolor laborum incididunt eiusmod do ex sint est adipisicing sunt esse. Fugiat mollit incididunt nulla velit sint sit ut consectetur ut laboris. Sint labore laborum quis sunt sunt labore do nisi. Dolore amet eu aliquip est nulla culpa non irure pariatur.",
+        "Vendly is an African platform that allows users to send secure packages (payments, degrees, gifts…) through social media. I worked on the backend for this startup and built most of the cloud functions. In addition, I built a part of the frontend.",
       stucks: [
         "next js",
         "material ui",
@@ -64,9 +64,9 @@ const projects = [
       "btsix.png",
     ],
     info: {
-      name: "btounsi",
+      name: "BTOUNSI",
       discreption:
-        "Sunt aute nulla aliquip elit. Elit ex nisi sint qui aute Lorem eiusmod duis sunt officia. Lorem amet dolor laborum incididunt eiusmod do ex sint est adipisicing sunt esse. Fugiat mollit incididunt nulla velit sint sit ut consectetur ut laboris. Sint labore laborum quis sunt sunt labore do nisi. Dolore amet eu aliquip est nulla culpa non irure pariatur.",
+        "In my beloved country Tunisia, we still don’t have a good streaming platform for our local TV shows. Therefore, I took the responsibility to build one and solve this problem, and this project was the solution that I provided. Btounsi is a single web application that interacts with youtube API to get the necessary shows from each original owner's channel (to protect their copyrights). Any user can filter shows by channel, year, or type to find his best show.",
       stucks: [
         "gatsby js",
         "bootstrap",
@@ -82,9 +82,9 @@ const projects = [
     id: 5,
     preview: ["eoone.png", "eotwo.png"],
     info: {
-      name: "ettissalet othmani",
+      name: "ETTISSALET OTHMANI",
       discreption:
-        "Sunt aute nulla aliquip elit. Elit ex nisi sint qui aute Lorem eiusmod duis sunt officia. Lorem amet dolor laborum incididunt eiusmod do ex sint est adipisicing sunt esse. Fugiat mollit incididunt nulla velit sint sit ut consectetur ut laboris. Sint labore laborum quis sunt sunt labore do nisi. Dolore amet eu aliquip est nulla culpa non irure pariatur.",
+        "A tech store asked me to build its website to show the products and sell them online. Therefore, this website allows the tech store employees to add new products using a content management system and allows user to search for the perfect product for them and buy it online.",
       stucks: [
         "gatsby js",
         "bootstrap",
